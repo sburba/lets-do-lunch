b548c91d3e6ddff2e815ad533e01a775a113626c
Sam Burba <sam.burba@learninga-z.com>
2015-06-05 14:14:37 -0400

    Allow posts to events

--- back/src/main/java/com/letsdolunch/letsdolunch/LunchEventController.java
+++ back/src/main/java/com/letsdolunch/letsdolunch/LunchEventController.java
@@ -2,16 +2,17 @@ package com.letsdolunch.letsdolunch;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @EnableAutoConfiguration
 public class LunchEventController {
-    @RequestMapping("/lunchevent/{id}")
+    @RequestMapping(value = "/events", method = RequestMethod.POST)
     @ResponseBody
-    long home(@PathVariable long id) {
-        return id;
+    String home(@RequestParam String person, @RequestParam String location, @RequestParam String time) {
+        return String.format("Person: %s, Location: %s, Time: %s.", person, location, time);
     }
 }
