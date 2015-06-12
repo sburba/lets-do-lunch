462a1e1ccdf0a2f0d811ea118acac6dbe129a3a4
John Fonte <john@fonte.com>
2015-06-12 16:06:03 -0400

    getters/setters

--- src/main/java/com/letsdolunch/letsdolunch/model/User.java
+++ src/main/java/com/letsdolunch/letsdolunch/model/User.java
@@ -28,6 +28,20 @@ public class User {
     this.name = name;
   }
 
-  // getter and setter methods ...
-  
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
 } // class User
