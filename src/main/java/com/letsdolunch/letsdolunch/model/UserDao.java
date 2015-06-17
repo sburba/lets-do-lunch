62b40e2dc1ce00cfc9f3bdd5f798790dc82fbfd3
John Fonte <john@fonte.com>
2015-06-17 11:56:46 -0400

    uhh

--- src/main/java/com/letsdolunch/letsdolunch/model/UserDao.java
+++ src/main/java/com/letsdolunch/letsdolunch/model/UserDao.java
@@ -5,7 +5,7 @@ import javax.transaction.Transactional;
 import org.springframework.data.repository.CrudRepository;
 
 @Transactional
-public interface UserDao extends CrudRepository<User, Int> {
+public interface UserDao extends CrudRepository<User, Integer> {
 
   /**
    * This method is not implemented and its working code will be
